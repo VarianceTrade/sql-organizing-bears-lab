900ad80bb9e4870b3c174d646a0ac8de108551dd
Brandon <sugb73@gmail.com>
2021-03-16 14:44:14 -0400

    commit tree 08b0dc2b5c02505e5e8085e74c582132acf7b5c5

---
+++ lib/insert.sql
@@ -0,0 +1,19 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr Chocolate", 5, "M", "brown", "cool", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 3, "M", "black", "sassy", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 2, "F", "brown", "easy", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, "Sergeant Brown", 8, "M", "black", "awesome", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Mr Chocolate", 5, "M", "brown", "cool", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Rowdy", 3, "M", "white", "sassy", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Tabitha", 2, "F", "brown", "easy", 1);
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, null, 8, "M", "black", "awesome", 1);
+
+-- SELECT name, age FROM bears WHERE sex="F"
+
+-- SELECT name FROM bears ORDER BY name ASC
